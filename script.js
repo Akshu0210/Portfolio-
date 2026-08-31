5878f00e8c66eecd72c9736f8229ff74eca8b3fa
Akshu0210 <akshaya.dk05@gmail.com>
2026-08-31 12:46:46 +0530

    Add theme toggle functionality in script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+const themeBtn = document.getElementById("themeBtn");
+
+themeBtn.addEventListener("click", function () {
+
+    document.body.classList.toggle("dark");
+
+    if (document.body.classList.contains("dark")) {
+        themeBtn.textContent = "☀️";
+    } else {
+        themeBtn.textContent = "🌙";
+    }
+
+});
